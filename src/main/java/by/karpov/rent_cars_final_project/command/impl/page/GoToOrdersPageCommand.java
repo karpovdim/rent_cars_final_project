a3d9e8e72cfab8d5ae745e66c0e3ab29256fa596
karpovdim <karpov83d@gmail.com>
2022-02-05 21:29:43 +0300

    added payment command, service

---
+++ src/main/java/by/karpov/rent_cars_final_project/command/impl/page/GoToOrdersPageCommand.java
@@ -0,0 +1,62 @@
+package by.karpov.rent_cars_final_project.command.impl.page;
+
+import by.karpov.rent_cars_final_project.command.Command;
+import by.karpov.rent_cars_final_project.command.PagePath;
+import by.karpov.rent_cars_final_project.command.RequestParameter;
+import by.karpov.rent_cars_final_project.controller.Router;
+import by.karpov.rent_cars_final_project.entity.Car;
+import by.karpov.rent_cars_final_project.entity.Order;
+import by.karpov.rent_cars_final_project.entity.User;
+import by.karpov.rent_cars_final_project.exception.ServiceException;
+import by.karpov.rent_cars_final_project.service.CarService;
+import by.karpov.rent_cars_final_project.service.OrderService;
+import by.karpov.rent_cars_final_project.service.impl.CarServiceImpl;
+import by.karpov.rent_cars_final_project.service.impl.OrderServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.Map;
+
+import static by.karpov.rent_cars_final_project.command.SessionAttribute.*;
+
+public class GoToOrdersPageCommand implements Command {
+	private static final Logger logger = LogManager.getLogger();
+	private static final int LIMIT_ORDERS_ON_PAGE = 3;
+	private final OrderService orderService = OrderServiceImpl.getInstance();
+
+	@Override
+	public Router execute(HttpServletRequest request) {
+		logger.log(Level.INFO, "method execute()");
+		Router router;
+		HttpSession session = request.getSession();
+		session.setAttribute(PREVIOUS_PAGE, PagePath.ORDERS_PAGE_REDIRECT);
+		String page = request.getParameter(RequestParameter.CURRENT_PAGE_NUMBER);
+		int currentPageNumber;
+		if (page != null) {
+			currentPageNumber = Integer.parseInt(page);
+		} else {
+			currentPageNumber = 1;
+		}
+		int leftBorderCars = (LIMIT_ORDERS_ON_PAGE * (currentPageNumber - 1));
+		User user = (User) session.getAttribute(USER);
+		try {
+			double numberOfOrders = orderService.countOrders(user.getId());
+			double maxNumberOfPages = Math.ceil(numberOfOrders / LIMIT_ORDERS_ON_PAGE);
+			List<Order> orders = orderService.findByUserIdAndLimit(user.getId(), leftBorderCars, LIMIT_ORDERS_ON_PAGE);
+			session.setAttribute(CURRENT_PAGE_NUMBER, currentPageNumber);
+			session.setAttribute(MAX_NUMBER_OF_PAGES, maxNumberOfPages);
+			session.setAttribute(RequestParameter.LIST_ORDERS, orders);
+
+			router = new Router(PagePath.ORDERS_PAGE);
+		} catch (ServiceException e) {
+			logger.log(Level.ERROR, "error on orders page: ", e);
+			router = new Router(PagePath.ERROR_500_PAGE);
+		}
+		return router;
+	}
+
+}
